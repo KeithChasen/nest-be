52aa117abcd286bd6cc2ce584ac57849e1fe928f
keithchasen <keithchasen89@gmail.com>
2026-02-21 21:56:32 +0100

    task resource / simple task route

---
+++ src/task/task.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  findAll() {
+    return [
+      { id: 1, title: 'Task 1', isCompleted: false },
+      { id: 2, title: 'Task 2', isCompleted: true },
+    ];
+  }
+}
